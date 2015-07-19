2089da0657a8a964a82f3eb03061be7de586adfd
Juan Pablo Buritica <buritica@gmail.com>
2015-07-18 20:59:01 -0400

    [refactor] DRY up debugging

--- app/services/stripe.js
+++ app/services/stripe.js
@@ -2,16 +2,9 @@
 import env from '../config/environment';
 import Ember from 'ember';
 
-/**
- * Uses Ember.Logger.info to output service information if LOG_STRIPE_SERVICE is
- * set
- *
- * notes:
- * - proxies all arguments to Ember.Logger.info
- * - pre-pends StripeService to all messages
- */
 function debug() {
   var debuggingEnabled = (typeof env.LOG_STRIPE_SERVICE !== 'undefined');
+  console.log('debuggingEnabled', debuggingEnabled);
 
   if (!debuggingEnabled) {
     return false;
@@ -22,14 +15,11 @@ function debug() {
   Ember.Logger.info.apply(null, args);
 }
 
-/**
- * Creates a creditCard token using Stripe.js API, exposed as `card.createToken`
- * @param  {ojbect} card  CreditCard
- * @return {promise}      Returns a promise that holds response, see stripe.js docs for details
- *                        status is not being returned at the moment but it can be logged
- */
 function createCardToken (card) {
   debug('card.createToken:', card);
+
+  // manually start Ember loop
+  Ember.run.begin();
 
   return new Ember.RSVP.Promise(function (resolve, reject) {
     Stripe.card.createToken(card, function (status, response) {
@@ -46,15 +36,20 @@ function createCardToken (card) {
   });
 }
 
-/**
- * Creates a BankAccout token using Stripe.js API, exposed as `bankAccount.createToken`
- * @param  {ojbect} bankAccount
- * @return {promise}      Returns a promise that holds response, see stripe.js docs for details
- *                        Status is not being returned at the moment but it can be logged
- *
- */
+function createCardTokenDeprecated(card) {
+  Ember.deprecate(
+    '`EmberStripeService.createToken` has been deprecated in ' +
+    'favor of `EmberStripeService.card.createToken` to match ' +
+    'the Stripe API.'
+  );
+  return createCardToken(card);
+}
+
 function createBankAccountToken(bankAccount) {
   debug('bankAccount.createToken:', bankAccount);
+
+  // manually start Ember loop
+  Ember.run.begin();
 
   return new Ember.RSVP.Promise(function (resolve, reject) {
     Stripe.bankAccount.createToken(bankAccount, function (status, response) {
